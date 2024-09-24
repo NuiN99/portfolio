8f6439b48c851abb9c71ac715977722d6c11850d
NuiN99 <endyverse@gmail.com>
2024-09-23 20:08:12 -0700

    feat: update some parts with new style

--- src/App.js
+++ src/App.js
@@ -4,97 +4,55 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './app/globals.css';
 import React from 'react';
 import { Container, Row, Col, Button, Card, Nav, Tab } from 'react-bootstrap';
+import Colours from './colours.js';
 import Projects from './app/projects/projects.json';
 import Image from 'next/image';
 
-const TopSection = () => {
+const Navbar = () => {
   return (
     <>
-      <div className='bg-dark px-3 py-1'>
-        <div className='m-5'>
-          <Name className='text-light'>Cai Plank</Name>
-          <HorizontalDivider className='bg-secondary' />
-          <SubName className='text-light'>Game Programmer</SubName>
-        </div>
-      </div>
-
-      <TabButtons />
+      <HeroColorWrapper>
+        <HorizontalMarginLarge className='p-3'>
+          <NameText>Cai Plank</NameText>
+          <TitleText className='text-light'>Game Programmer</TitleText>
+        </HorizontalMarginLarge>
+      </HeroColorWrapper>
     </>
   )
 }
 
-const TabButtons = () => {
+const ProjectsSection = () => {
   return (
-    <Tab.Container id="bottom-tabs-example" defaultActiveKey="Games">
-      <Row>
-        <Col>
-          <div className='bg-dark w-100'>
-            <Nav fill variant="tabs bg-dark border-0 w-50 mx-auto" defaultActiveKey="Games">
-              <Nav.Item className='mx-1'>
-                <Nav.Link eventKey="Games" className='text-dark bg-light'>Games</Nav.Link>
-              </Nav.Item>
-              <Nav.Item className='mx-1'>
-                <Nav.Link eventKey="Tools" className='text-dark bg-light'>Tools</Nav.Link>
-              </Nav.Item>
-              <Nav.Item className='mx-1'>
-                <Nav.Link eventKey="About" className='text-dark bg-light'>About</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </div>
-
-          <Tab.Content className='bg-light'>
-            <Tab.Pane eventKey="Games">
-              <GameShowcases />
-            </Tab.Pane>
-            <Tab.Pane eventKey="Tools">
-
-            </Tab.Pane>
-            <Tab.Pane eventKey="About">
-
-            </Tab.Pane>
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
+    <BGColorWrapper>
+      <HorizontalMarginLarge>
+      <div className='d-flex flex-wrap justify-content-center'>
+        {
+          Projects.games.map((details, index) => {
+            return (
+              <ProjectCard details={details} key={index}></ProjectCard>
+            );
+          })
+        }
+      </div>
+      </HorizontalMarginLarge>
+    </BGColorWrapper>
   )
 }
 
-
-const ShowcaseCard = ({ details }) => {
-  const [showGIFCover, setShowGIFCover] = React.useState(true);
-
+const ProjectCard = ({ details }) => {
   return (
-    <Card className='flex-shrink-0 m-3' border='info' style={{ width: '30rem' }}>
+    <Card className='flex-shrink-0 m-3' style={{ width: '30rem' }}>
       <Card.Body>
-        <Card.Title>{details.title}</Card.Title>
-
-        {
-          showGIFCover == true ?
-            <Image
-              src={details.gifCover}
-              width={0}
-              height={0}
-              sizes="100vw"
-              style={{ width: '100%', height: 'auto' }}
-              alt="Image Loading..."
-              onMouseEnter={() => setShowGIFCover(false)} />
-            : null
-        }
-
-        {
-          showGIFCover == false ?
-            <Image
+        <Image
               src={details.gif}
               width={0}
               height={0}
               sizes="100vw"
               style={{ width: '100%', height: 'auto' }}
               alt="GIF Loading..."
-              onMouseLeave={() => setShowGIFCover(true)}
-            />
-            : null
-        }
-
+        />
+        <div className='py-1'></div>
+        <Card.Title>{details.title}</Card.Title>
         <Card.Text>{details.description}</Card.Text>
         <a href={details.link} target="_blank">
           <Button variant="primary">itch.io</Button>
@@ -104,42 +62,32 @@ const ShowcaseCard = ({ details }) => {
   )
 }
 
-
-const GameShowcases = () => {
-  return (
-    <div className='d-flex flex-wrap bg-light'>
-      {
-        Projects.games.map((details, index) => {
-          return (
-            <ShowcaseCard details={details} key={index}></ShowcaseCard>
-          );
-        })
-      }
-    </div>
-  )
-}
-
-
 const App = () => {
   return (
     <>
-      <TopSection />
+      <Navbar />
+      <ProjectsSection />
     </>
   )
 }
 
-
-const TopSectionBackerSmall = styled.div`
-  height: 5rem;
+const HeroColorWrapper = styled.div`
+  background-color: ${Colours.HERO};
 `
 
-const Name = styled.div`
-  font-size: 3rem;
+const BGColorWrapper = styled.div`
+  background-color: ${Colours.BG};
+`
+
+const NameText = styled.div`
+  font-size: 4rem;
   font-weight: bold;
+  color: ${Colours.ORANGE};
+  line-height: 1em;
 `
 
-const SubName = styled.div`
-  font-size: 2rem;
+const TitleText = styled.div`
+  font-size: 1.5rem;
 `
 
 const HorizontalDivider = styled.div`
@@ -147,8 +95,9 @@ const HorizontalDivider = styled.div`
   border-radius: 3rem;
 `
 
-const ImageTest = styled(Image)`
-  object-fit: contain;
+const HorizontalMarginLarge = styled.div`
+  margin-right: 256px;
+  margin-left: 256px;
 `
 
 
