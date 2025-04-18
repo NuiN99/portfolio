f114c10c45c7d6136b4f23ca2b058c133cebf156
NuiN99 <endyverse@gmail.com>
2025-04-18 15:13:46 -0700

    fix: scrollbar and too long project labels

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,21 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Cai Plank | Game Programmer',
   description: "Cai Plank's game programming portfolio.",
+  viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
